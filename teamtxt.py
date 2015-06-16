87fb5a5864af35453d893147f9fa56b2a8bd7e2e
Phil <plantlife26@gmail.com>
2015-06-15 21:33:31 -0400

    team text output

--- teamtxt.py
+++ teamtxt.py
@@ -1,13 +1,49 @@
 import csv
 
-def printTeam(team):
-    with open('FanGraphs/FanGraphsPitching/2014_pitching_master.csv', 'rU') as csvfile:
+batting_players = []
+batting_output = []
+batting_text = []
+
+pitching_players = []
+pitching_output = []
+
+
+def printBatting(team):
+    with open('FanGraphs/FanGraphsBatting/2014_batting_master.csv', 'rU') as csvfile:
+        reader = csv.DictReader(csvfile)
+        
+        team_text = open("team.txt", "w")
+
+        for row in reader:
+            if (row["Team"] == team):
+                # print(row["Name"], row["H"], row["IPR"])
+                batting_players.append(([row["Name"], float(row["H"]), float(row["IPR"])]))
+
+
+    batting_output = sorted(batting_players, key=lambda x: x[1], reverse=True)
+   
+
+    for x in batting_output:
+        print x
+        team_text.write("{} {} {} \n".format(x[0],x[1],x[2])) 
+    team_text.close
+	
+
+def printPitching(team):
+    with open('FanGraphs/FanGraphsPitching/2014_pitching_master_withoutPercentage.csv', 'rU') as csvfile:
         reader = csv.DictReader(csvfile)
 
         for row in reader:
             if (row["Team"] == team):
-                print(row["Team"], row["Name"], row["HR"])
+                # print(row["Name"], row["H"], row["IPR"])
+                pitching_players.append((row["Name"], float(row["G"]), (float(row["FB%"])), (float(row["FBv"])), (float(row["SL%"])), (float(row["SLv"])), (float(row["CT%"])), (float(row["CTv"])), (float(row["CB%"])), (float(row["CBv"])), (float(row["CH%"])), (float(row["CHv"])), (float(row["SF%"])), (float(row["SFv"])), (float(row["KN%"])), (float(row["KNv"])), (float(row["XX%"]))))
 
-printTeam("Mets")
-printTeam("Giants")
-printTeam("Red Sox")
+    pitching_output = sorted(pitching_players, key=lambda x: x[1], reverse=True)
+
+    for x in pitching_output:
+        print x
+
+printBatting("Mets")
+
+
+printPitching("Yankees")
