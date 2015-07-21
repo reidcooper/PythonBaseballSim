1f0882d399912b445ebd20dd2cf1d4e24cdddbe6
Phil <plantlife26@gmail.com>
2015-07-21 10:32:27 -0400

    added UML, and test json data to json folder, removed old team python file

--- baseballGamePackage/Game.py
+++ baseballGamePackage/Game.py
@@ -92,5 +92,5 @@ class Game(object):
   self.gameEventList.extend(aList) 
 
  def getJSONData(self):
-  with open('data.txt' , 'w') as outfile:
+  with open('data.json' , 'w') as outfile:
 	  json.dump(self.gameEventList, outfile, sort_keys = True, indent = 4, ensure_ascii = False)
